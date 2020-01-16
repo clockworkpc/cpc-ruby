13b223dea9a8f7c5c110690bcd936cc19db6d3c3
alexandergarber <agarber@profectusgroup.com>
2020-01-16 16:46:42 +1100

    unit testing express

--- lib/cpc/api/my_restful_api.rb
+++ lib/cpc/api/my_restful_api.rb
@@ -14,15 +14,18 @@ module Cpc
         api_get_request(args_hsh)
       end
 
-      def get_contact
+      def get_all_contacts
         args_hsh = { url: { host: @host, path: 'contact' } }
         api_get_request(args_hsh)
       end
 
+      def get_contact(id_str)
+        args_hsh = { url: { host: @host, path: 'contact', _id: id_str } }
+        api_get_request(args_hsh)
+      end
+
       def post_contact(contact_hsh)
-
         request_body_uri = URI.encode(contact_hsh.map {|k,v| "#{k}=#{v}"}.join('&'))
-
         args_hsh = {
           url: { host: @host, path: 'contact' },
           request_body: request_body_uri
@@ -30,14 +33,27 @@ module Cpc
         api_post_request(args_hsh)
       end
 
-      def put_contact(contactID_str)
-        args_hsh = { url: { host: @host, path: 'contact', contact_id: contactID_str } }
+      def put_contact(contact_hsh)
+        request_body_uri = URI.encode(contact_hsh.map {|k,v| "#{k}=#{v}"}.join('&'))
+        args_hsh = {
+          url: { host: @host, path: 'contact', _id: contact_hsh[:_id] },
+          request_body: request_body_uri
+        }
         api_put_request(args_hsh)
       end
 
       def delete_contact(contactID_str)
         args_hsh = { url: { host: @host, path: 'contact', contact_id: contactID_str } }
         api_delete_request(args_hsh)
+      end
+
+      def delete_latest_contact(protected_id_ary = nil)
+        all_contacts = get_all_contacts.body
+        deletable_contacts = all_contacts.reject { |h| protected_id_ary.include?(h['_id']) }
+        deletable_ids = deletable_contacts.map { |h| h['_id'] }
+        # expect(deletable_ids.count).to eq(all_contacts.count - 2)
+        id_to_delete = deletable_ids.last
+        delete_contact(id_to_delete)
       end
     end
   end
